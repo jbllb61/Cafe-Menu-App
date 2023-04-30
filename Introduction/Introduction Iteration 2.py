f0f108a5ae1df8df56de10faf3bf2d75acafe22c
jbllb61 <129808826+jbllb61@users.noreply.github.com>
2023-04-30 23:48:23 +0000

    new file:   Introduction/Introduction Iteration 2.py

---
+++ Introduction/Introduction Iteration 2.py
@@ -0,0 +1,13 @@
+print("Welcome to the school cafe app!")
+
+while True:
+    try:
+        age = int(input("Please enter your age: "))
+        if age < 13 or age > 18:
+            print("Sorry, this app is only available for students aged 13-18.")
+            exit()
+        break  # if age is valid, exit the loop
+    except ValueError:
+        print("Invalid input. Please enter a valid number. eg 13 or 15")
+
+print("Please log in or create an account to use the app.")
