da55b48a173dcb81934775d76fa38b52f028b03f
jbllb61 <129808826+jbllb61@users.noreply.github.com>
2023-05-02 00:58:25 +0000

    modified:   Signing up/Sign up iteration 2.py

--- Signing up/Sign up iteration 2.py
+++ Signing up/Sign up iteration 2.py
@@ -2,7 +2,7 @@ import os
 
 # create user_details.txt if it doesn't exist
 if not os.path.exists("user_details.txt"):
-    with open("user_details.txt", "w") as user_file:
+    with open("user_details.txt", "w") as f:
         pass
 
 # login and signup options
@@ -11,8 +11,8 @@ while True:
     if option == '1':
         username = input("Enter your username: ")
         password = input("Enter your password: ")
-        with open("user_details.txt", "r") as user_file:
-            for line in user_file:
+        with open("user_details.txt", "r") as f:
+            for line in f:
                 line = line.strip().split(",")
                 if line[0] == username and line[1] == password:
                     print(f"Welcome back {username}! What would you like to order?")
@@ -23,8 +23,8 @@ while True:
         new_username = input("Enter your desired username: ")
         new_password = input("Enter your desired password: ")
         # TODO: Add age verification for new users
-        with open("user_details.txt", "a") as user_file:
-            user_file.write(f"{new_username},{new_password}\n")
+        with open("user_details.txt", "a") as f:
+            f.write(f"{new_username},{new_password}\n")
         print("Sign up successful! Please log in with your new credentials.")
     else:
         print("Invalid input. Please enter either '1' or '2'.")
