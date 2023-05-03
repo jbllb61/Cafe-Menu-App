c09d18ffdbb27c30055ce97976fa30a2ef917012
jbllb61 <129808826+jbllb61@users.noreply.github.com>
2023-05-03 03:09:49 +0000

    modified:   Signing up/Sign up iteration 2.py modified:   user_details.txt

--- Signing up/Sign up iteration 2.py
+++ Signing up/Sign up iteration 2.py
@@ -1,11 +1,9 @@
 import os
-
-# create user_details.txt if it doesn't exist
+ # create user_details.txt if it doesn't exist
 if not os.path.exists("user_details.txt"):
     with open("user_details.txt", "w") as user_file:
         pass
-
-# login and signup options
+ # login and signup options
 while True:
     option = input("Enter '1' to log in, '2' to sign up: ")
     if option == '1':
@@ -22,11 +20,21 @@ while True:
         if line[0] == username and line[1] == password:
             break  # end the loop after logging in successfully
     elif option == '2':
-        new_username = input("Enter your desired username: ")
-        new_password = input("Enter your desired password: ")
-        # TODO: Add age verification for new users
-        with open("user_details.txt", "a") as user_file:
-            user_file.write(f"{new_username},{new_password}\n")
-        print("Sign up successful! Please log in with your new credentials.")
+        while True:
+            new_username = input("Enter your desired username: ")
+            new_password = input("Enter your desired password: ")
+            # check if username already exists
+            with open("user_details.txt", "r") as user_file:
+                for line in user_file:
+                    line = line.strip().split(",")
+                    if line[0] == new_username:
+                        print("This username already exists. Please choose a different username.")
+                        break
+                else:
+                    # add new user to file if username is unique
+                    with open("user_details.txt", "a") as user_file:
+                        user_file.write(f"{new_username},{new_password}\n")
+                    print("Sign up successful! Please log in with your new credentials.")
+                    break  # end the loop after successful registration
     else:
         print("Invalid input. Please enter either '1' or '2'.")
