2d28cbf9eee311133e8971cfe85bb1ee4d296902
jbllb61 <129808826+jbllb61@users.noreply.github.com>
2023-05-12 10:33:19 +1200

    Update Menu Iteration 3.py

--- Menu/Menu Iteration 3.py
+++ Menu/Menu Iteration 3.py
@@ -30,29 +30,43 @@ while True:
         continue
     item_name = list(ITEMS.keys())[choice - 1]
     if item_name in order:
-        order[item_name] += quantity  # Update the quantity if the item is already in the order
+        order[item_name]['quantity'] += quantity  # Update the quantity if the item is already in the order
     else:
-        order[item_name] = quantity  # Add the item and its quantity to the order
+        order[item_name] = {'quantity': quantity, 'gluten-free': False, 'dairy-free': False}  # Add the item and its quantity to the order
+
+    gluten_free = input('Is this item gluten-free? (y/n): ')
+    if gluten_free.lower().strip() == 'y' or gluten_free.lower().strip() == 'yes':
+        order[item_name]['gluten-free'] = True
+    
+    dairy_free = input('Is this item dairy-free? (y/n): ')
+    if dairy_free.lower().strip() == 'y' or dairy_free.lower().strip() == 'yes':
+        order[item_name]['dairy-free'] = True
 
 # Display the order summary as a receipt
-print('\n' + '-'*30)
-print('{:^30}'.format('ORDER SUMMARY'))
-print('-'*30)
-for item, quantity in order.items():
+print('\n' + '-'*45)
+print('{:^45}'.format('ORDER SUMMARY'))
+print('-'*45)
+for item, details in order.items():
     if item not in ITEMS:
         continue
     price = ITEMS[item]
+    quantity = details['quantity']
+    gluten_free = details['gluten-free']
+    dairy_free = details['dairy-free']
+    item_name = item.title()
+    if gluten_free:
+        item_name += ' (gluten-free)'
+    if dairy_free:
+        item_name += ' (dairy-free)'
     subtotal = price * quantity
-    print('{:<20} x{:>2}  ${:>6.2f}'.format(item.title(), quantity, subtotal))
-print('-'*30)
-total = sum(ITEMS[item] * order[item] for item in order)
-print('{:<20}  ${:>6.2f}'.format('TOTAL', total))
-print('-'*30)
+    print('{:<30} x{:>2}  ${:>6.2f}'.format(item_name, quantity, subtotal))
+print('-'*45)
+total = sum(ITEMS[item] * order[item]['quantity'] for item in order)
+print('{:<30}  ${:>6.2f}'.format('TOTAL', total))
+print('-'*45)
 
 feedback_text = input("Please provide your feedback: ")
 
 # Check if feedback is not empty and has length greater than 5
 if feedback_text.strip() != "" and len(feedback_text) > 5:
     print("Thank you for your feedback!")
-else:
-    print("Thank you for your time!")
