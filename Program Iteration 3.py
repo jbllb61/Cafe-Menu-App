d3e38feb3fc8a04fd8ee7b2eefc8f9198b895112
jbllb61 <129808826+jbllb61@users.noreply.github.com>
2023-05-15 12:06:00 +1200

    implemented menu iteration 3 to program iteration3

--- Program Iteration 3.py
+++ Program Iteration 3.py
@@ -101,29 +101,85 @@ while True:
         continue
     item_name = list(ITEMS.keys())[choice - 1]
     if item_name in order:
-        order[item_name] += quantity  # Update the quantity if the item is already in the order
+        order[item_name]['quantity'] += quantity  # Update the quantity if the item is already in the order
     else:
-        order[item_name] = quantity  # Add the item and its quantity to the order
+        order[item_name] = {'quantity': quantity, 'gluten-free': False, 'dairy-free': False}  # Add the item and its quantity to the order
+
+    gluten_free = input(f'Is {item_name.title()} gluten-free? (y/n): ')
+    if gluten_free.lower().strip() == 'y' or gluten_free.lower().strip() == 'yes':
+        order[item_name]['gluten-free'] = True
+    
+    dairy_free = input(f'Is {item_name.title()} dairy-free? (y/n): ')
+    if dairy_free.lower().strip() == 'y' or dairy_free.lower().strip() == 'yes':
+        order[item_name]['dairy-free'] = True
 
 # Display the order summary as a receipt
-print('\n' + '-'*30)
-print('{:^30}'.format('ORDER SUMMARY'))
-print('-'*30)
-for item, quantity in order.items():
+print('\n' + '-'*57)
+print('{:^57}'.format('ORDER SUMMARY'))
+print('-'*57)
+item_num = 1
+for item, details in order.items():
     if item not in ITEMS:
         continue
     price = ITEMS[item]
+    quantity = details['quantity']
+    gluten_free = details['gluten-free']
+    dairy_free = details['dairy-free']
+    item_name = item.title()
+    if gluten_free:
+        item_name += ' (gluten-free)'
+    if dairy_free:
+        item_name += ' (dairy-free)'
     subtotal = price * quantity
-    print('{:<20} x{:>2}  ${:>6.2f}'.format(item.title(), quantity, subtotal))
-print('-'*30)
-total = sum(ITEMS[item] * order[item] for item in order)
-print('{:<20}  ${:>6.2f}'.format('TOTAL', total))
-print('-'*30)
+    print('{:<5}{:<30} x {:>2}  ${:>6.2f}'.format(item_num, item_name, quantity, subtotal))
+    item_num += 1
+print('-'*57)
+total = sum(ITEMS[item] * order[item]['quantity'] for item in order)
+print('{:<5}{:<30}      ${:>6.2f}'.format('', 'TOTAL', total))
+print('-'*57)
+
+# Remove items from the order
+while True:
+    choice_remove = input('Enter an item number to remove (or "done" to continue): ')
+    if choice_remove == 'done':
+        break
+    try:
+        choice_remove = int(choice_remove)
+    except ValueError:
+        print('Sorry, that is not a valid item number. Please try again.')
+        continue
+    if choice_remove not in range(1, len(order) + 1):
+        print('Sorry, that item number is not available.')
+        continue
+    item_name_remove = list(order.keys())[choice_remove - 1]
+    del order[item_name_remove]
+    # Display the updated order summary
+    print('\n' + '-'*57)
+    print('{:^57}'.format('UPDATED ORDER SUMMARY'))
+    print('-'*57)
+    item_num = 1
+    for item, details in order.items():
+        if item not in ITEMS:
+            continue
+        price = ITEMS[item]
+        quantity = details['quantity']
+        gluten_free = details['gluten-free']
+        dairy_free = details['dairy-free']
+        item_name = item.title()
+        if gluten_free:
+            item_name += ' (gluten-free)'
+        if dairy_free:
+            item_name += ' (dairy-free)'
+        subtotal = price * quantity
+        print('{:<5}{:<30} x {:>2}  ${:>6.2f}'.format(item_num, item_name, quantity, subtotal))
+        item_num += 1
+    print('-'*57)
+    total = sum(ITEMS[item] * order[item]['quantity'] for item in order)
+    print('{:<30}  ${:>6.2f}'.format('TOTAL', total))
+    print('-'*57)
 
 feedback_text = input("Please provide your feedback: ")
 
 # Check if feedback is not empty and has length greater than 5
 if feedback_text.strip() != "" and len(feedback_text) > 5:
-    print("Thank you for your time!")
-else:
     print("Thank you for your feedback!")
